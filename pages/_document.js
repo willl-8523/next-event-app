b84ebd06aa505bce91817d1788a3c5dccf992ed4
willl85 <ngowilfried@gmail.com>
2023-12-05 15:49:52 +0100

    Add link to load icon

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="images/events-logo.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
