535ef8ebc9fd1118f52ee6e90d8aeacac960b6aa
willl85 <ngowilfried@gmail.com>
2023-12-06 21:09:49 +0100

    Custom new-event page

---
+++ pages/events/new-event.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewEvent from '../../components/events/NewEvent';
+import Home from '../../components/home-page/Home';
+
+export default function NewEventPage() {
+  return (
+    <>
+      <NewEvent />
+      <Home />
+    </>
+  );
+}
