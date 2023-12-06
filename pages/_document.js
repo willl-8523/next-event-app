4b0afc25fbb8ff92c6481141cf7fe780d2c97698
willl85 <ngowilfried@gmail.com>
2023-12-06 21:13:42 +0100

    Add div#modal to create modal

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="icon" type="image/svg+xml" href="images/events-logo.png" />
       </Head>
       <body>
+        <div id="modal"></div>
         <Main />
         <NextScript />
       </body>
