135275a78586886bb8bf378576fe266a2445bf30
willl85 <ngowilfried@gmail.com>
2023-12-06 16:02:33 +0100

    Add conditional content Header

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -1,14 +1,27 @@
 import React, { Fragment } from 'react';
 import Header from './Header';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Layout(props) {
+  const router = useRouter();
+  const pathId = router.query.eventId;
+  let content = (
+    <Link legacyBehavior href="/events/new">
+      <a className="button">New Event</a>
+    </Link>
+  );
+  if (pathId) {
+    content = (
+      <Link legacyBehavior href="/events">
+        <a className="nav-item">View All Events</a>
+      </Link>
+    );
+  }
   return (
     <Fragment>
       <Header>
-        <Link legacyBehavior href="/events/new">
-          <a className="button">New Event</a>
-        </Link>
+        {content}
       </Header>
       <main>{props.children}</main>
     </Fragment>
