35da3f4a9a0566299b899cf99d8a9dba8a767626
willl85 <ngowilfried@gmail.com>
2023-12-06 21:15:18 +0100

    Build and Re-use Home Component

---
+++ components/home-page/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './Hero';
+import LastEvents from './LastEvents';
+import SearchEvent from './SearchEvent';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <LastEvents />
+      <SearchEvent />
+    </>
+  );
+}
